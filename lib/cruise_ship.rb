3caa03d3eada22631f290a6b9fffb82bcbd2a8da
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 01:56:46 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
 end
+select_winner({
+suite_a: "Amanda Presley",
+ suite_b: "Seymour Hoffman",
+ suite_c: "Alfred Tennyson",
+ suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+ })
