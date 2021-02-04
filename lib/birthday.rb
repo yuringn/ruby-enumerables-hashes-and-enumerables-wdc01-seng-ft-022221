13921716368e19f66224d0af8aeb2fc31d406470
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 01:54:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,12 @@
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each {|name,age|}
+  puts "Happy birthday #{name}! You are now #{age} years old!"
   # add your code snippet here!
 end
+happy_birthday({
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+})
